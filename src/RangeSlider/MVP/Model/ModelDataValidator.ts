8e49d3960a02cd193352201f28a0417dee7c655f
Nikita Khakimov <53759907+grzdrz@users.noreply.github.com>
2020-09-21 06:35:07 +0700

    Раздробил длинное условие на методы #3

--- src/RangeSlider/MVP/Model/ModelDataValidator.ts
+++ src/RangeSlider/MVP/Model/ModelDataValidator.ts
@@ -11,75 +11,6 @@ class ModelDataValidator {
     this.model = model;
   }
 
-  public validateValues(data: IModelData): void {
-    const wasSliderParametersChanged = data.stepSize !== undefined || data.maxValue !== undefined || data.minValue !== undefined;
-    if (wasSliderParametersChanged) {
-      if (data.values !== undefined) this.model.data.values = data.values;
-      this.model.data.values = this.model.data.values.map((value, index) => this.validateValue(value, index, true));
-      this.valuesChanged();
-    } else if (data.values !== undefined && data.values.length > 0) {
-      // проверяем был ли сдвинут какой либо ползунок
-      let changedValueIndex = -1;
-      let deltaDirection = 0;
-      data.values.forEach((e, i) => {
-        if (e !== this.model.data.values[i]) changedValueIndex = i;
-      });
-      if (changedValueIndex !== -1) deltaDirection = data.values[changedValueIndex] - this.model.data.values[changedValueIndex];
-      const wasHandleMove = (changedValueIndex !== -1 && deltaDirection !== 0);
-
-      this.model.data.values = data.values;
-
-      if (this.model.data.values.length > 1 && wasHandleMove) {
-        if (deltaDirection > 0) { // протаскивание всех ползунков после текущего
-          this.model.data.values[changedValueIndex] = this.validateValue(this.model.data.values[changedValueIndex], changedValueIndex, this.model.data.canPush);
-          for (let i = changedValueIndex + 1; i < this.model.data.values.length; i += 1) {
-            this.model.data.values[i] = this.validateValue(this.model.data.values[i], i, !this.model.data.canPush);
-          }
-        } else { // протаскивание всех ползунков до текущего
-          this.model.data.values[changedValueIndex] = this.validateValue(this.model.data.values[changedValueIndex], changedValueIndex, this.model.data.canPush);
-          for (let i = changedValueIndex - 1; i >= 0; i -= 1) {
-            this.model.data.values[i] = this.validateValue(this.model.data.values[i], i, !this.model.data.canPush);
-          }
-        }
-      } else if (this.model.data.values.length === 1) {
-        this.model.data.values[0] = this.validateValue(this.model.data.values[0], 0, false);
-      }
-      this.valuesChanged();
-    }
-  }
-
-  public valuesChanged(): void {
-    const viewData = this.model.getViewData();
-    const { filledStrips } = viewData;
-    const newFilledStrips = new Array<boolean>();
-    for (let i = 0; i < this.model.data.values.length + 1; i += 1) {
-      if (i < filledStrips.length) {
-        newFilledStrips.push(filledStrips[i]);
-      } else {
-        newFilledStrips.push(false);
-      }
-    }
-
-    this.model.onSetViewData.invoke(new EventArgs<IViewData>({ filledStrips: newFilledStrips }));
-  }
-
-  public validateValue(value: number, countNumber: number, canPush: boolean): number {
-    const newTargetInputValue = this.calculateNearestPositionForHandle(value);
-
-    const { values } = this.model.data;
-
-    if (newTargetInputValue > values[countNumber + 1] && !canPush) return values[countNumber + 1];
-    if (newTargetInputValue < values[countNumber - 1] && !canPush) return values[countNumber - 1];
-    return this.validateGoingOutOfBounds(newTargetInputValue);
-  }
-
-  public validateGoingOutOfBounds(value: number): number {
-    const { minValue, maxValue } = this.model.data;
-    if (value < minValue) return minValue;
-    if (value > maxValue) return maxValue;
-    return value;
-  }
-
   public validateMaxValue(stepSize: number, maxValue: number): number {
     const dMaxMin = maxValue - this.model.data.minValue;
     if (dMaxMin <= 0) return this.model.data.minValue + stepSize;
@@ -106,10 +37,95 @@ class ModelDataValidator {
     return this.model.data.maxValue - stepSize * roundedValueOfOneStep;
   }
 
+  public validateValues(data: IModelData): void {
+    const wasSliderParametersChanged = data.stepSize !== undefined || data.maxValue !== undefined || data.minValue !== undefined;
+    if (wasSliderParametersChanged) {
+      if (data.values !== undefined) this.model.data.values = data.values;
+      this.model.data.values = this.model.data.values.map((value, index) => this.validateValue(value, index, true));
+      this.updateFilledStrips();
+    } else if (data.values !== undefined && data.values.length > 0) {
+      const changedValueIndex = this.findMovedHandle(data.values);
+      const valueShift = changedValueIndex !== -1 ? data.values[changedValueIndex] - this.model.data.values[changedValueIndex] : 0;
+      this.model.data.values = data.values;
+      this.moveHandle(changedValueIndex, valueShift);
+      this.updateFilledStrips();
+    }
+  }
+
+  private moveHandle(changedValueIndex: number, valueShift: number): void {
+    const wasHandleMoved = changedValueIndex !== -1 && valueShift !== 0;
+    const needMoveMoreThanOneHandle = this.model.data.values.length > 1 && wasHandleMoved;
+    const needMoveOneHandle = this.model.data.values.length === 1;
+
+    if (needMoveMoreThanOneHandle) {
+      this.model.data.values[changedValueIndex] = this.validateValue(
+        this.model.data.values[changedValueIndex],
+        changedValueIndex,
+        this.model.data.canPush,
+      );
+      this.pushHandles(changedValueIndex, valueShift);
+    } else if (needMoveOneHandle) {
+      this.model.data.values[0] = this.validateValue(this.model.data.values[0], 0, false);
+    }
+  }
+
+  private pushHandles(changedValueIndex: number, valueShift: number): void {
+    const needPushForward = valueShift > 0;
+    if (needPushForward) {
+      for (let i = changedValueIndex + 1; i < this.model.data.values.length; i += 1) {
+        this.model.data.values[i] = this.validateValue(this.model.data.values[i], i, !this.model.data.canPush);
+      }
+    } else {
+      for (let i = changedValueIndex - 1; i >= 0; i -= 1) {
+        this.model.data.values[i] = this.validateValue(this.model.data.values[i], i, !this.model.data.canPush);
+      }
+    }
+  }
+
+  private findMovedHandle(values: number[]): number {
+    let changedValueIndex = -1;
+    values.forEach((e, i) => {
+      if (e !== this.model.data.values[i]) changedValueIndex = i;
+    });
+    return changedValueIndex;
+  }
+
+  private updateFilledStrips(): void {
+    const viewData = this.model.getViewData();
+    const { filledStrips } = viewData;
+    const newFilledStrips = new Array<boolean>();
+    for (let i = 0; i < this.model.data.values.length + 1; i += 1) {
+      if (i < filledStrips.length) {
+        newFilledStrips.push(filledStrips[i]);
+      } else {
+        newFilledStrips.push(false);
+      }
+    }
+
+    this.model.onSetViewData.invoke(new EventArgs<IViewData>({ filledStrips: newFilledStrips }));
+  }
+
+  private validateValue(value: number, countNumber: number, canPush: boolean): number {
+    const newTargetInputValue = this.calculateNearestPositionForHandle(value);
+
+    const { values } = this.model.data;
+
+    if (newTargetInputValue > values[countNumber + 1] && !canPush) return values[countNumber + 1];
+    if (newTargetInputValue < values[countNumber - 1] && !canPush) return values[countNumber - 1];
+    return this.validateGoingOutOfBounds(newTargetInputValue);
+  }
+
+  private validateGoingOutOfBounds(value: number): number {
+    const { minValue, maxValue } = this.model.data;
+    if (value < minValue) return minValue;
+    if (value > maxValue) return maxValue;
+    return value;
+  }
+
   // подменяем текущее значение инпута на число к которому ближе всего текущее значение курсора
   // т.е. например шаг 10, значение 78 -> на выходе получаем 80,
   // или например  шаг 10, значение 73 -> на выходе получаем 70
-  public calculateNearestPositionForHandle(value: number): number {
+  private calculateNearestPositionForHandle(value: number): number {
     let absoluteValue;
     let resultValue;
     const { minValue, stepSize } = this.model.data;
